0d4f17d743e2f608dc4a76c9a57efe8d799d1a1d
mariyamnavila <bibimariyamnavila@gmail.com>
2025-08-22 21:00:00 +0600

    added navbar ,footer,errorpage

---
+++ src/Components/Dashboard/Dashboard.jsx
@@ -0,0 +1,10 @@
+
+const Dashboard = () => {
+    return (
+        <div>
+            <h2>dscbhdc</h2>
+        </div>
+    );
+};
+
+export default Dashboard;
--- src/Components/Root/Root.jsx
+++ src/Components/Root/Root.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import Navbar from '../Navbar/Navbar';
+import { Outlet } from 'react-router-dom';
+import Footer from '../Footer/Footer';
 
 const Root = () => {
     return (
         <div>
-            <h3>Helloooooo</h3>
+            <Navbar></Navbar>
+            <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
